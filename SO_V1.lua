a49bf6faf82e34a031b6b6b563eb9e5e5ed1312b
oiim10 <mohammadrohim567@gmail.com>
2026-09-16 02:50:31 +0700

    Update SO_V1.lua

--- SO_V1.lua
+++ SO_V1.lua
@@ -49173,7 +49173,7 @@ end
 local choice = gg.choice(
     labels,
     nil,
-    "            🎯SELECT GOLDPASS\n━━━━━━━━━━━━━━━━━\n✅AVAILABLE NOW : HARVEST GOLDPASS"
+    "            🎯SELECT GOLDPASS\n━━━━━━━━━━━━━━━━━━━━━\n✅AVAILABLE NOW : HARVEST GOLDPASS"
 )
 
 if not choice then
